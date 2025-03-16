f1984f14919d53d052e707911098d6867ed89306
tamer tamer <amr.dawoodi@hotmail.com>
2025-03-16 09:59:06 +0300

    add env location

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   },
   swcMinify: true,
   reactStrictMode: true,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
 };
 
 export default nextConfig;
